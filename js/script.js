a908efd01cda77a3fc37131058314143a57afe2d
nrj14 <nourdine.arji@gmail.com>
2014-09-15 16:00:45 +0100

    scope chaine

--- js/script.js
+++ js/script.js
@@ -1,22 +1,2 @@
-var WorldCtrl = function ($scope) {
-	// $scope.population = 7000;
-
-	var countries = [];
-	var countCountries = 30;
-	for(var n = 0; n < countCountries; n++){
-		countries.push({
-			name: 'Country '+n, 
-			population :  Math.random()*300 + 10 //  Math.rand()*100 +
-		});
-	}
-
-	$scope.countries = countries;
-	$scope.population = function (){
-		var population = 0;
-		countries.forEach(function(country){
-			population += country.population;
-		});
-		return population;
-	};
-} 
-
+var HelloCtrl = function ($scope) {
+};
