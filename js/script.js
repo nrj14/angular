9d8caa739b653e43351e901d99b13b3637e3e254
nrj14 <nourdine.arji@gmail.com>
2014-09-15 19:34:55 +0100

    a complet form example with more ng-directives: ng-class, ng-disable

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
+var FormController = function($scope) {
 
+	$scope.message = "msg";
+	$scope.remaining = 100 - $scope.message.length;
+	$scope.remainingStyle = {};
+
+	$scope.$watch('message', function(newVal, oldVal) {
+		if(newVal != oldVal){
+			// if($scope.message.length > 100){
+			// 	$scope.message = $scope.message.slice(0,99);
+			// }
+			$scope.remaining = Math.max(0, 100 - $scope.message.length)  ;
+		}
+	})
+
+	$scope.overflow = function() {
+		return $scope.message.length > 100;
+	};
+	$scope.empty = function() {
+		return $scope.message.length == 0;
+	};
+	$scope.clear = function() {
+		$scope.message = "";
+	};
+
+	$scope.send = function() {
+		console.log("button send !")
+	};
+
+};
 
 
 
