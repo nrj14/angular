403a7cb441b391988134e338a79d44aed588c003
nrj14 <nourdine.arji@gmail.com>
2014-09-15 15:39:00 +0100

    WorldCtrl

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-// Code goes here
+var WorldCtrl = function ($scope) {
+	// $scope.population = 7000;
 
-function hController($scope){
-  $scope.name = "world";
-}
+	var countries = [];
+	var countCountries = 30;
+	for(var n = 0; n < countCountries; n++){
+		countries.push({
+			name: 'Country '+n, 
+			population :  Math.random()*300 + 10 //  Math.rand()*100 +
+		});
+	}
+
+	$scope.countries = countries;
+	$scope.population = function (){
+		var population = 0;
+		countries.forEach(function(country){
+			population += country.population;
+		});
+		return population;
+	};
+} 
 
